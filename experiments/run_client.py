9069c9c59999a595e4873509d2e9e0cbaf91db1b
Koukyosyumei <koukyosyumei@hotmail.com>
2021-06-26 10:09:45 +0900

    fix play function

--- experiments/run_client.py
+++ experiments/run_client.py
@@ -1,14 +1,27 @@
-from tentohako.agent import RandomAgent
+import argparse
+
+from tentohako.agent import MinMaxAgent, RandomAgent
 from tentohako.socket import Client
 
 HOST_PORT = 8020
 
 
-def main():
-    agent = RandomAgent()
+def main(agent):
     client = Client(agent, HOST_PORT)
     client.play()
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", help="the type of your agent",
+                        type=str)
+    args = parser.parse_args()
+
+    if args.a == "r":
+        agent = RandomAgent()
+    elif args.a == "m":
+        agent = MinMaxAgent()
+    else:
+        agent = RandomAgent()
+
+    main(agent)
