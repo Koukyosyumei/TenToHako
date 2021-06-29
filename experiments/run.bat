35246b78de635efe58fb5ce54565adfb878f08dc
Koukyosyumei <koukyosyumei@hotmail.com>
2021-06-29 14:25:16 +0900

    change #trial 30 -> 1

--- experiments/run.bat
+++ experiments/run.bat
@@ -1,5 +1,5 @@
-for /l %%n in (1,1,30) do (
-    start python run_server.py -p 8020 -c 2 -r 2 -o score_log.txt
+for /l %%n in (1,1,1) do (
+    start cmd /k python run_server.py -p 8020 -c 3 -r 3 -o score_log.txt
     start python run_client.py -a r -p 8020
-    python run_client.py -a m -p 8020
+    start python run_client.py -a r -p 8020
 )
