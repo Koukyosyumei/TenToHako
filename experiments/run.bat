98a7570abfd6ba70c2fabe5458fcc982ec30eeb2
Koukyosyumei <koukyosyumei@hotmail.com>
2021-06-30 20:17:10 +0900

    remove experiments folder

--- experiments/run.bat
+++
@@ -1,5 +0,0 @@
-for /l %%n in (1,1,20) do (
-    start python run_client.py -a r -p 8020
-    start python run_client.py -a u -p 8020
-    python run_server.py -p 8020 -c 2 -r 2 --lo score_log.txt -v 0 --go %%n.gif
-)
