f12490fed3dc33226499946b39efb9311bc27555
Koukyosyumei <koukyosyumei@hotmail.com>
2021-07-02 14:31:12 +0900

    this q-learning agent doesn't work

--- src/tentohako/agent/__init__.py
+++ src/tentohako/agent/__init__.py
@@ -1,4 +1,5 @@
 from .base import BaseAgent
 from .mixmax import MinMaxAgent
+from .qlearning import QLearningAgent
 from .random import RandomAgent
 from .uct import UCTAgent
--- src/tentohako/agent/qlearning.py
+++ src/tentohako/agent/qlearning.py
@@ -5,7 +5,7 @@ from .base import BaseAgent
 
 
 class QLearningAgent(BaseAgent):
-    def __init__(self, name="q-learning", e=0.1, alpha=0.3, gamma=0.9):
+    def __init__(self, name="q-learning", e=0.2, alpha=0.003, gamma=0.9):
         super().__init__(name)
         self.e = e
         self.alpha = alpha
@@ -14,13 +14,15 @@ class QLearningAgent(BaseAgent):
         self.q = {}
         self.last_move = None
         self.last_state = None
+        self.totalgamecount = 0
+        self.initial_q = 1
 
     def policy(self, board):
         board_matrix_string = str(board.board_matrix)
         self.last_state = copy.deepcopy(board_matrix_string)
         valid_actions = self.get_valid_action(board)
 
-        if random.random() < self.e:
+        if random.random() < (self.e / (self.totalgamecount // 10000 + 1)):
             picked_action = random.choice(valid_actions)
         else:
             qs = [self.get_Q(board_matrix_string, act)
@@ -38,23 +40,27 @@ class QLearningAgent(BaseAgent):
         return picked_action
 
     def get_Q(self, board_matrix_string, act):
-        if self.q.get((board_matrix_string, act)) is None:
-            self.q[(board_matrix_string, act)] = 1
+        if self.q.get((board_matrix_string, act, self.player_id)) is None:
+            self.q[(board_matrix_string, act, self.player_id)] = self.initial_q
 
-        return self.q.get((board_matrix_string, act))
+        return self.q.get((board_matrix_string, act, self.player_id))
 
-    def learn(self, board, id_to_scores, done):
+    def learn(self, board, id_to_scores):
         if self.last_move is not None:
             if not board.is_done():
                 self.update_Q(self.last_state, self.last_move, 0, board)
+
             else:
-                if id_to_scores[self.user_id] > id_to_scores[-1*self.user_id]:
+                if id_to_scores[str(self.player_id)] >\
+                        id_to_scores[str(-1*self.player_id)]:
                     self.update_Q(self.last_state, self.last_move, 1, board)
-                elif id_to_scores[self.user_id] < id_to_scores[-self.user_id]:
+                elif id_to_scores[str(self.player_id)] <\
+                        id_to_scores[str(-self.player_id)]:
                     self.update_Q(self.last_state, self.last_move, -1, board)
                 else:
                     self.update_Q(self.last_state, self.last_move, 0, board)
 
+                self.totalgamecount += 1
                 self.last_move = None
                 self.last_state = None
 
@@ -66,5 +72,5 @@ class QLearningAgent(BaseAgent):
             new_max_Q = max([self.getQ(str(fs.board_matrix), act)
                              for act in fs.get_valid_actions()])
 
-        self.q[(s, a)] = q_value + self.alpha * \
+        self.q[(s, a, self.player_id)] = q_value + self.alpha * \
             ((r+self.gamma*new_max_Q) - q_value)
