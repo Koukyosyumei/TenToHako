f203047beb00dbeb10844f5cb9d02ba4c48dc789
Koukyosyumei <koukyosyumei@hotmail.com>
2021-07-02 10:49:11 +0900

    modify run.bat

--- examples/run.bat
+++ examples/run.bat
@@ -1,5 +1,5 @@
 for /l %%n in (1,1,10) do (
-    start python run_client.py -a m -p 8020
+    start python run_client.py -a r -p 8020
     start python run_client.py -a u -p 8020
     python run_server.py -p 8020 -c 2 -r 2 --lo score_log.txt -v 1 --go %%n.gif
 )
