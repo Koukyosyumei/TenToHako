aedd101ff2b1d7dc593b761efac1fe4a44acf547
Koukyosyumei <koukyosyumei@hotmail.com>
2021-07-02 17:21:19 +0900

    add QLearningAgent to the experiments

--- examples/run_client.py
+++ examples/run_client.py
@@ -1,6 +1,6 @@
 import argparse
 
-from tentohako.agent import MinMaxAgent, RandomAgent, UCTAgent
+from tentohako.agent import MinMaxAgent, QLearningAgent, RandomAgent, UCTAgent
 from tentohako.socket import Client
 
 
@@ -23,6 +23,11 @@ if __name__ == '__main__':
         agent = MinMaxAgent()
     elif args.a == "u":
         agent = UCTAgent(timelimit=2)
+    elif args.a == "q":
+        agent = QLearningAgent()
+        agent.load(
+            "../saved_models/qlearning_ncol_3_nrow_3_scoremin_1_scoremax_9_iterations_3000.pickle")
+        agent.eval()
     else:
         agent = RandomAgent()
 
