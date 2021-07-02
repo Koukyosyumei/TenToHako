d340fa931c2ebc7f5892673617a8526f304c9388
Koukyosyumei <koukyosyumei@hotmail.com>
2021-07-02 16:10:27 +0900

    re-write the QLearningAgent

--- src/tentohako/agent/qlearning.py
+++ src/tentohako/agent/qlearning.py
@@ -1,76 +1,165 @@
-import copy
+import math
 import random
+from collections import defaultdict
+
+import numpy as np
 
 from .base import BaseAgent
 
 
 class QLearningAgent(BaseAgent):
-    def __init__(self, name="q-learning", e=0.2, alpha=0.003, gamma=0.9):
+    def __init__(self, name="q-learning", epsilon=0.2,
+                 alpha=0.4, discount=0.9):
         super().__init__(name)
-        self.e = e
+        self.epsilon = epsilon
         self.alpha = alpha
-        self.gamma = gamma
+        self.discount = discount
+        self._qvalues = defaultdict(lambda: defaultdict(lambda: 0))
 
-        self.q = {}
-        self.last_move = None
-        self.last_state = None
-        self.totalgamecount = 0
-        self.initial_q = 1
+    def adaptive_rate(self, t):
+        return 1.1 - max([0.1, min(1, math.log((t)/25))])
 
-    def policy(self, board):
-        board_matrix_string = str(board.board_matrix)
-        self.last_state = copy.deepcopy(board_matrix_string)
-        valid_actions = self.get_valid_action(board)
+    def get_qvalue(self, string_state, action):
+        """Returns Q(state, action)
 
-        if random.random() < (self.e / (self.totalgamecount // 10000 + 1)):
-            picked_action = random.choice(valid_actions)
+        Args:
+            string_state: string representation of the state
+            action: picked actino
+
+        Returns:
+            self._qvalues[string_state][action]: Q(state, action)
+        """
+        return self._qvalues[string_state][action]
+
+    def set_qvalue(self, string_state, action, value):
+        """Sets the Qvalue for [state, action] to the given value
+
+        Args:
+            string_state: string representation of the state
+            action: picked actino
+            value: QValue for (state, action)
+        """
+        self._qvalues[string_state][action] = value
+
+    def get_value(self, board):
+        """Compute your agent's estimate of V(s) using current q-values
+           V(s) = max_over_action Q(state, action) over possible actions.
+
+        Args:
+            board: the instance of Bard class
+
+        Returns:
+            value:estimated value of the state
+
+        Note:
+            please take into account that q-values can be negative.
+        """
+        possible_actions = self.get_valid_action(board)
+
+        # If there are no legal actions, return 0.0
+        if len(possible_actions) == 0:
+            return 0.0
+
+        string_state = str(board.board_matrix)
+        q_values = np.array([self.get_qvalue(string_state, action)
+                             for action in possible_actions])
+        value = np.max(q_values)
+
+        return value
+
+    def update(self, board, action, reward, next_board, t, adaptive=False):
+        """Update Q-value
+           Q(s, a) := (1 - alpha) * Q(s, a) + alpha * (r + gamma * V(s'))
+
+        Args:
+            board: the current state
+            action: action from the current state to the next state
+            reward: reward of the current state
+            next_board: next state
+            t: the current step
+            adaptive: use adapted learning rate or not
+        """
+
+        # agent parameters
+        gamma = self.discount
+        if adaptive:
+            learning_rate = self.alpha * self.adaptive_rate(t)
         else:
-            qs = [self.get_Q(board_matrix_string, act)
-                  for act in valid_actions]
-            q_max = max(qs)
-            best_actions = [valid_actions[i]
-                            for i, q in enumerate(qs) if q == q_max]
-            picked_action = random.choice(best_actions)
-            self.last_move = picked_action
+            learning_rate = self.alpha
 
-        return picked_action
+        string_state = str(board.board_matrix)
+        q_s_a = (1 - learning_rate) * self.get_qvalue(string_state, action) + \
+            learning_rate * (reward + gamma *
+                             self.get_value(next_board))
+
+        self.set_qvalue(string_state, action, q_s_a)
+
+    def get_best_action(self, board):
+        """Compute the best action to take in a state(using current q-values).
+
+        Args:
+            board: the current state
+
+        Returns:
+            best_action: the best action
+        """
+        possible_actions = self.get_valid_action(board)
+
+        # If there are no legal actions, return None
+        if len(possible_actions) == 0:
+            return None
+
+        string_state = str(board.board_matrix)
+        q_values = np.array([self.get_qvalue(string_state, action)
+                             for action in possible_actions])
+        best_index = np.argmax(q_values)
+        best_action = possible_actions[best_index]
+
+        return best_action
+
+    def get_action(self, board):
+        """Compute the action to take in the current state, including exploration.
+           With probability self.epsilon, we should take a random action.
+           otherwise - the best policy action (self.getPolicy).
+
+        Args:
+            board: the current state
+
+        Returns:
+            chosen_action: chosen action
+        """
+
+        # Pick Action
+        possible_actions = self.get_valid_action(board)
+
+        # If there are no legal actions, return None
+        if len(possible_actions) == 0:
+            return None
+
+        # agent parameters:
+        epsilon = self.epsilon
+
+        best_or_random = np.random.binomial(n=1, p=epsilon)
+
+        if best_or_random == 0:
+            chosen_action = self.get_best_action(board)
+        else:
+            chosen_action = random.choice(possible_actions)
+
+        return chosen_action
 
     def step(self, board, id_to_scores):
-        picked_action = self.policy(board)
+        """Return the action based on the given board.
+
+        Args:
+            board: the instance of Board class which represents
+                   the current board state.
+            id_to_scores: dictionary whose keies are the user id and the
+                          values are scores
+
+        Returns:
+            picked_action:
+        """
+
+        picked_action = self.get_action(board)
         return picked_action
-
-    def get_Q(self, board_matrix_string, act):
-        if self.q.get((board_matrix_string, act, self.player_id)) is None:
-            self.q[(board_matrix_string, act, self.player_id)] = self.initial_q
-
-        return self.q.get((board_matrix_string, act, self.player_id))
-
-    def learn(self, board, id_to_scores):
-        if self.last_move is not None:
-            if not board.is_done():
-                self.update_Q(self.last_state, self.last_move, 0, board)
-
-            else:
-                if id_to_scores[str(self.player_id)] >\
-                        id_to_scores[str(-1*self.player_id)]:
-                    self.update_Q(self.last_state, self.last_move, 1, board)
-                elif id_to_scores[str(self.player_id)] <\
-                        id_to_scores[str(-self.player_id)]:
-                    self.update_Q(self.last_state, self.last_move, -1, board)
-                else:
-                    self.update_Q(self.last_state, self.last_move, 0, board)
-
-                self.totalgamecount += 1
-                self.last_move = None
-                self.last_state = None
-
-    def update_Q(self, s, a, r, fs):
-        q_value = self.get_Q(s, a)
-        if fs.is_done:
-            new_max_Q = 0
-        else:
-            new_max_Q = max([self.getQ(str(fs.board_matrix), act)
-                             for act in fs.get_valid_actions()])
-
-        self.q[(s, a, self.player_id)] = q_value + self.alpha * \
-            ((r+self.gamma*new_max_Q) - q_value)
