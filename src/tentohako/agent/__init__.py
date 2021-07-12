b39832676315d75b73a0e45b36aa451eafb6f862
Koukyosyumei <koukyosyumei@hotmail.com>
2021-07-12 14:56:14 +0900

    add alphabeta agent

--- src/tentohako/agent/__init__.py
+++ src/tentohako/agent/__init__.py
@@ -1,5 +1,5 @@
 from .base import BaseAgent
-from .mixmax import MinMaxAgent
+from .mixmax import AlphaBetaAgent, MinMaxAgent
 from .qlearning import QLearningAgent
 from .random import RandomAgent
 from .uct import UCTAgent
