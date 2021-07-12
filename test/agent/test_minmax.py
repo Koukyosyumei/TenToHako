fdee608a97c6c7b7d0389356dcd12957cbb09e1e
Koukyosyumei <koukyosyumei@hotmail.com>
2021-07-12 14:57:01 +0900

    add test for alphabeta agent

--- test/agent/test_minmax.py
+++ test/agent/test_minmax.py
@@ -31,3 +31,38 @@ def test_minmaxagent():
     assert picked_action == (2, 1)
     assert minmaxscore_depth_1 == 8
     assert minmaxscore_depth_2 == 3
+
+
+def test_alphabetaagent():
+    from tentohako.agent import AlphaBetaAgent
+    from tentohako.game import Board
+
+    board_matrix = [
+        ["*", "-", "*", "-", "*"],
+        ["|", 8, "|", 2, "|"],
+        ["*", " ", "*", " ", "*"],
+        [" ", 5, "|", 3, "|"],
+        ["*", "-", "*", "-", "*"],
+    ]
+    name = "alphabeta"
+    depth = 2
+    player_id = 1
+    id_to_scores = {"1": 0, "-1": 0}
+
+    board = Board([], 2, 2)
+    board.initialize()
+    board.board_matrix = board_matrix
+
+    alphabetaagent = AlphaBetaAgent(name, depth=depth)
+    alphabetaagent.set_player_id(player_id)
+    valid_actions = alphabetaagent.get_valid_action(board)
+    picked_action = alphabetaagent.step(board, id_to_scores)
+    alphabetaagent_depth_1 = alphabetaagent.minmax(board, player_id, id_to_scores, 1)
+    alphabetaagent_depth_2 = alphabetaagent.minmax(board, player_id, id_to_scores, 2)
+
+    assert alphabetaagent.name == name
+    assert alphabetaagent.player_id == player_id
+    assert valid_actions == [(2, 1), (2, 3), (3, 0)]
+    assert picked_action == (2, 1)
+    assert alphabetaagent_depth_1 == 8
+    assert alphabetaagent_depth_2 == 3
